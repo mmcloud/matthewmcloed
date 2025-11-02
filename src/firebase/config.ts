0fd88ce728d7a3e7356bdf6b09024a9b7c9e2a3e
Matthew McLeod <mtthwmcld@gmail.com>
2025-11-02 08:56:10 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1330206715-c9a5d",
+  "appId": "1:860772345281:web:dba9f2f08a059425eebb40",
+  "apiKey": "AIzaSyAe2Gh2boH-leJi-OIoEStRgilAoWF-7Rw",
+  "authDomain": "studio-1330206715-c9a5d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "860772345281"
+};
